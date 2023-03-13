5e93ff902cd548df2bf7d118e29726000ab3b370
김밍슈 <kmsu2046@gmail.com>
2023-03-13 23:09:19 +0900

    Login

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const loginform = document.querySelector("#login-form");
 const loginInput = loginform.querySelector("input");
-
+const greeting = document.querySelector("#greeting");
 
 const link = document.querySelector("#Chrome-app-clone-coding-with-VanillaJS");
 
 function onLoginSubmit(event) {
+    event.preventDefault();
+    loginform.classList.add("hidden");
     const username = loginInput.value;
-    event.preventDefault();
-    console.log(username);
+    localStorage.setItem("username", username);
+    paintGreetings(savedUsername);
 }
 
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.log(event);
-    alert("clicked");
+function paintGreetings(username){
+    greeting.innerText = `hello ${savedUsername}`;
+    greeting.classList.remove("hidden");
 }
 
-loginform.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+const savedUsername = localStorage.getItem("username");
+
+if(savedUsername === null){
+    loginform.classList.remove("hidden");
+    loginform.addEventListener("submit", onLoginSubmit);
+} else {
+   paintGreetings(savedUsername);
+}
