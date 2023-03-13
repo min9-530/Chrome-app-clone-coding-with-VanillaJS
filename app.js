4c2d6666247676ec22771b8effbd1993c33dd30d
김밍슈 <kmsu2046@gmail.com>
2023-03-13 20:17:49 +0900

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const loginform = document.querySelector("#login-form");
 const loginInput = loginform.querySelector("input");
 
+
+const link = document.querySelector("#Chrome-app-clone-coding-with-VanillaJS");
+
 function onLoginSubmit(event) {
     const username = loginInput.value;
     event.preventDefault();
     console.log(username);
 }
+
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+    alert("clicked");
+}
+
 loginform.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
