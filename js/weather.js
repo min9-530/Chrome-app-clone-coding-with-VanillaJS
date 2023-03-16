eeaa4adf0c73c4f10d34fb58d33a5f42b94de2d0
김밍슈 <kmsu2046@gmail.com>
2023-03-16 21:16:00 +0900

    location

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,10 @@
+const API_KEY ="07e0e3407d72fd4668645eedaa4ba133";
+
 function onGeo0k(position){
     const lat =position.coords.latitude;
-    const lng =position.coords.longitude;
-    console.log("You live in ", lat, lng);
+    const lon =position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => );
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.");
